3ccfc381c3760fa77e1a08a22bff70a51e85c9c9
Therese C Nordnes <therese@getacademy.no>
2024-11-29 13:32:28 +0100

    Initial commit

---
+++ ChatBot.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_RS_TCN
+{
+    public class ChatBot
+    {
+        public string Name { get; set; }
+ 
+        List<string> answers = new List<string>();
+        List<ChatBot> bots = new List<ChatBot>();
+
+        public ChatBot(string name) { 
+            Name = name;
+
+        }
+        public ChatBot() { }
+
+
+        public void menu()
+        {
+            Console.WriteLine("Hei, velkommen til chatBot, nå kan du lage deg en chatbot eller snakke med en som finnes, Tast 1 for å lage, 2 for å snakke med en");
+            string option = Console.ReadLine();
+            checkTask(option);
+
+        }
+        void checkTask(string option)
+        {
+            switch (option)
+            {
+                case "1":
+                    createBot();
+                    menu();
+
+                    break;
+
+                case "2":
+               
+                    TalkToBot();
+
+                    break;
+            }
+        }
+        public void createBot()
+        {
+            Console.WriteLine(" skriv inn ønsket navn på Boten din");
+            string chatBotName = Console.ReadLine();
+            ChatBot chatBot = new ChatBot(chatBotName);
+            bots.Add(chatBot);
+            Console.WriteLine($"Du har laget en bot som heter: {chatBot.Name}");
+         
+         
+            AddAnswer();
+           
+        
+
+         
+        }
+
+        void AddAnswer()
+        {
+            Console.WriteLine("skriv inn ett svar som chatbot kan svare:");
+            string answer = Console.ReadLine();
+            answers.Add(answer);
+            Console.WriteLine("Vil du legge til flere svar? Y/N");
+            string a =Console.ReadLine();
+            checkIfMoreAnswer(a);
+
+        }
+
+        void checkIfMoreAnswer(string moreAnswerOrNot)
+        {
+            if (moreAnswerOrNot == "Y")
+            {
+                AddAnswer();
+            }
+            else if (moreAnswerOrNot == "N")
+            {
+                {
+                    menu();
+                }
+            }
+        }
+
+
+
+        public void TalkToBot()
+        {
+            Console.WriteLine($"Du snakker nå med {bots[0].Name}, hva vil du spørre om ");
+            Console.ReadLine();
+
+            Random random = new Random();
+
+            int randomNr = random.Next(0, answers.Count);
+
+            Console.WriteLine(answers[randomNr]);
+            Console.WriteLine("vil du fortsette å snakke med meg? N/Y");
+            string a =Console.ReadLine();
+
+            if(a == "Y")
+            {
+                TalkToBot();
+            }
+            else
+            {
+                menu();
+            }
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//2) Chættbått’n
+//Du skal lage en app hvor man kan lage sin egen chatbot - og deretter velge å snakke med den 🙂
+//Brukeren skal få prompt på chættbått navn, og deretter mulighet til å skrive inn forskjellige
+//tilbakemeldinger/ svar som chættbått’n skal trekke et tilfeldig svar fra og svare brukeren med når den startes opp.
+//Bruk gjerne en meny for å gå inn i de forskjellige modusene: “mekke en chættbått” og “preke med en chættbått”
+
+using PP_RS_TCN;
+using System.ComponentModel.Design;
+
+ChatBot bot = new ChatBot();
+bot.menu();
+
+
+
+
+
+
+
+
+//Console.WriteLine("skriv inn ett svar som chatbot kan svare:");
+//string one = Console.ReadLine();
+
+//Console.WriteLine("skriv inn ett svar til som chatbot kan svare:");
+//string two = Console.ReadLine();
+
+//Console.WriteLine("skriv inn ett svar til som chatbot kan svare:");
+//string three = Console.ReadLine();
+
+//Console.WriteLine($"{chatBot.Name} Still meg ett spm");
+//string question = Console.ReadLine();
+
+//Random random = new Random();
+
+//int randomNr = random.Next(1,4);
+
+//switch (randomNr)
+//{
+//    case 1: Console.WriteLine($"spm: {question}, svaret er:   {one}"); break;
+//    case 2: Console.WriteLine($"spm: {question}, svaret er:   {two}"); break;
+//    case 3: Console.WriteLine($"spm: {question}, svaret er:   {three}"); break;
+//}
+
+
+
+
+
